5f5a7f34d88452fe4930ff1a50904872d27a2aaa
PabloAbara <pbabara@uc.cl>
2018-03-19 13:32:51 -0300

    Last Changes
    Agregué fecha y eliminé la posibildad de "registrarse"

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :entries do
     resources :comments, only: [:create, :destroy, :update]
   end
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, except: [:registration_path]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html'
+
 
   root 'entries#index'
 end
